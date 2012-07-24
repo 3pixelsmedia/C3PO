b7f5c1d455102f1c6692a90b759d4f5b95bcab44
cromanelli <christianromanelli@gmail.com>
2012-07-23 19:56:45 -0430

    actualizacion headers

--- inc/head.php
+++ inc/head.php
@@ -3,8 +3,8 @@
 <title><?php echo $title ?></title>
 
 <link rel="stylesheet" href="css/reset.css" type="text/css">
-<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" title="Base style" charset="utf-8">
-<link rel="stylesheet" href="css/look.css" type="text/css" media="screen" title="style doc" charset="utf-8">
-<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" title="style doc" charset="utf-8">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" charset="utf-8">
+<link rel="stylesheet" href="css/look.css" type="text/css" media="screen"  charset="utf-8">
+<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen"  charset="utf-8">
 <script type="text/javascript" src="js/jquery-1.7.2.min.js" charset="utf-8"></script>
 <script type="text/javascript" src="js/behavior.js" charset="utf-8"></script>
